8dcd7b7b5c2919e718e22d4ebd481201f2340ca6
“Nilde <nilde.benedetti@gmail.com>
2026-09-07 12:10:10 +0200

    fix typos and add dynamic rout for project detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import MainLayout from "./layouts/MainLayout";
-import HomePage from "./pages/HomePage";
-import NotFound from "./pages/NotFound";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import ProjectDetailPage from "./pages/ProjectDetailPage.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route element={<MainLayout />}>
             <Route index element={<HomePage />} />
+            <Route path="projects/:id" element={<ProjectDetailPage />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
